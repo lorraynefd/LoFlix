ee3d0945c6f2806f6dfd1dc934d3c52612ad41af
lorraynefd <lorrayneferreiradias15@gmail.com>
2020-09-20 23:56:29 -0300

    diminuicao da description

--- loflix/src/components/FeaturedMovied.js
+++ loflix/src/components/FeaturedMovied.js
@@ -9,6 +9,10 @@ export default ({ item }) => {
   for (let i in item.genres) {
     genres.push(item.genres[i].name);
   }
+  let description = item.overview;
+  if(description.length > 200){
+    description = description.substring(0, 200)+'...';
+  }
   return (
     <section className="featured" style={{
       backgroundSize: 'cover',
@@ -23,7 +27,7 @@ export default ({ item }) => {
             <div className="featured--year">{firstDate.getFullYear()}</div>
             <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
           </div>
-          <div className="featured--description">{item.overview}</div>
+          <div className="featured--description">{description}</div>
           <div className="featured--buttons">
             <a href={`/watch/${item.id}`} className="featured--watchbutton">Assistir</a>
             <a href={`/list/add/${item.id}`} className="featured--mylistbutton">Minha Lista</a>
